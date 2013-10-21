eed7f9f46fbb7b54f2ece0c7de587885e903456c
Lonka <lonka.liu@gmail.com>
2013-10-21 10:32:23 +0800

    1. 加入Fixed Menu

--- SafetyCorner/Scripts/main.js
+++ SafetyCorner/Scripts/main.js
@@ -3,28 +3,41 @@ $(function () {
         $(window).bind('scroll resize', function () {
             var $this = $(this);
             var $this_Top = $this.scrollTop();
-            if ($this_Top > 167) {
+            if ($this_Top > 172) {
                 $('.menu').addClass('menu-fixed');
+                SetMainWidth('menu-fixed');
                 $('.menu-bg').show();
+                SetMainWidth('menu-bg');
+                $('.content').addClass('content-fixed');
             } else {
                 $('.menu').removeClass('menu-fixed');
                 $('.menu-bg').hide();
+                $('.content').removeClass('content-fixed');
             }
         });
+        SetMainWidth('main');
     });
 
+    $(window).resize(function () {
+        SetMainWidth('main');
+        SetMainWidth('menu-fixed');
+        SetMainWidth('menu-bg');
+        SetMainWidth('menu');
+    });
+
+    //#region Menu
     var submenu_active = new Array();
     $('.menu-title').mouseenter(function () {
         var menu_Id = $(this).attr("id");
         var id = menu_Id.replace('menu', '');
         submenu_active[id] = true;
-        var postion  = $(this).position();
-        $('div[for='+menu_Id+']').css({left:postion.left+5,top:postion.top+60}).fadeIn();
+        var postion = $(this).position();
+        $('div[for=' + menu_Id + ']').css({ left: postion.left + 5, top: postion.top + 60 }).fadeIn();
     }).mouseleave(function () {
         var menu_Id = $(this).attr("id");
         var id = menu_Id.replace('menu', '');
         submenu_active[id] = false;
-        setTimeout(function () { if (submenu_active[id] === false) $('div[for='+menu_Id+']').fadeOut(); }, 400);
+        setTimeout(function () { if (submenu_active[id] === false) $('div[for=' + menu_Id + ']').fadeOut(); }, 400);
     });
 
     $('.sub-menu').mouseenter(function () {
@@ -35,26 +48,30 @@ $(function () {
         var menu_Id = $(this).attr("for");
         var id = menu_Id.replace('menu', '');
         submenu_active[id] = false;
-        setTimeout(function () { if (submenu_active[id] === false) $('div[for='+menu_Id+']').fadeOut(); }, 400);
+        setTimeout(function () { if (submenu_active[id] === false) $('div[for=' + menu_Id + ']').fadeOut(); }, 400);
     });
+    //#endregion
+
+    function SetMainWidth(className) {
+        $('.' + className).width($(window).width() - 180);
+    }
+
+
+
+
 
 });
 
 
-function SetCwinHeight()
-{
-var iframeid=document.getElementById("content"); //iframe id
-  if (document.getElementById)
-  {   
-   if (iframeid && !window.opera)
-   {   
-    if (iframeid.contentDocument && iframeid.contentDocument.body.offsetHeight)
-     {   
-       iframeid.height = iframeid.contentDocument.body.offsetHeight;   
-     }else if(iframeid.Document && iframeid.Document.body.scrollHeight)
-     {   
-       iframeid.height = iframeid.Document.body.scrollHeight;   
-      }   
+function SetCwinHeight() {
+    var iframeid = document.getElementById("content"); //iframe id
+    if (document.getElementById) {
+        if (iframeid && !window.opera) {
+            if (iframeid.contentDocument && iframeid.contentDocument.body.offsetHeight) {
+                iframeid.height = iframeid.contentDocument.body.offsetHeight;
+            } else if (iframeid.Document && iframeid.Document.body.scrollHeight) {
+                iframeid.height = iframeid.Document.body.scrollHeight;
+            }
+        }
     }
-   }
 }

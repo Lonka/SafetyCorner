0b748eecec33eae06d60b2cdb3e043dbba9c6d4d
Lonka <lonka.liu@gmail.com>
2013-10-22 13:24:01 +0800

    1. 加入會議記錄上傳檔案

---
+++ SafetyCorner/Controllers/ConferenceRecondController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace SafetyCorner.Controllers
+{
+    public class ConferenceRecondController : Controller
+    {
+        //
+        // GET: /ConferenceRecond/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/Files/ConferenceReconds"), fileName);
+                file.SaveAs(path);
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
